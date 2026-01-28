e674a4d6aaade5984376b353ecb0f6f0cc808ce5
Kevin Watters <kevinwatters@gmail.com>
2026-01-28 17:22:46 +0100

    Add 02_compute.jai example.

--- run_examples.sh
+++ run_examples.sh
@@ -1,2 +1,8 @@
-pushd examples
-jai -quiet 01_memory.jai +Autorun; popd
+#!/bin/bash -e
+
+pushd examples || exit 1
+trap popd EXIT
+for example in 01_memory 02_compute; do
+    echo "Running ${example}..."
+    jai -quiet ${example}.jai && ./${example}
+done
