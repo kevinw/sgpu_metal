71f04504957927e8b4cb5c3d1a239fbbd5aa2fd3
Kevin Watters <kevinwatters@gmail.com>
2026-01-28 19:24:17 +0100

    Working 02_compute.

--- run_examples.sh
+++ run_examples.sh
@@ -2,6 +2,10 @@
 
 pushd examples || exit 1
 trap popd EXIT
+
+# Set library path for slang
+export DYLD_LIBRARY_PATH="../modules/slang/mac:${DYLD_LIBRARY_PATH}"
+
 for example in 01_memory 02_compute; do
     echo "Running ${example}..."
     jai -quiet ${example}.jai && ./${example}
