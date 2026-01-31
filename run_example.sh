334479ca7dca01dc3d1c6e3bc18bcab516bca4ab
Kevin Watters <kevinwatters@gmail.com>
2026-01-31 19:28:45 +0100

    example leak tracking

---
+++ run_example.sh
@@ -0,0 +1,8 @@
+name="$*"
+export MallocStackLogging=1
+export MallocStackLoggingNoCompact=1
+# export NSZombieEnabled=YES
+export NSAutoreleaseFreedObjectCheckEnabled=YES
+export OBJC_DEBUG_MISSING_POOLS=YES
+export OBJC_DEBUG_POOL_DEPTH=10
+cd examples && MTL_DEBUG_LAYER=1 jai -quiet $name.jai && leaks --quiet --atExit -- ./$name
